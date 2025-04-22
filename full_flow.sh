73cfe39e49c23ee2c3f2c9e8b785c3ba70b5174c
hhtnghia321 <38842210+hhtnghia321@users.noreply.github.com>
2025-04-22 15:29:20 +0700

    Update full_flow.sh

--- full_flow.sh
+++ full_flow.sh
@@ -3,16 +3,16 @@ echo "------------- Updating system and installing dependencies..."
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg git  # Ensure git is installed
 
-# Install Docker and Docker Compose
-echo "------------- Installing Docker and Docker Compose..."
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# # Install Docker and Docker Compose
+# echo "------------- Installing Docker and Docker Compose..."
+# sudo install -m 0755 -d /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+#   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
