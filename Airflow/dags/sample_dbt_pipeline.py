056400b8907de7ad3b3cfc502a7822ed949bdf28
hhtnghia321 <38842210+hhtnghia321@users.noreply.github.com>
2025-04-23 17:35:35 +0700

    Update sample_dbt_pipeline.py

--- Airflow/dags/sample_dbt_pipeline.py
+++ Airflow/dags/sample_dbt_pipeline.py
@@ -11,6 +11,6 @@ with DAG("dbt_run_in_project", schedule_interval=None, default_args=default_args
     dbt_run = BashOperator(
         task_id="run_dbt",
         bash_command="""
-        docker exec -it dbt_trino bash -c "cd /usr/app/dbt/raffle_shop && dbt-ol run --select query --target postgres2"
+        docker exec dbt_trino bash -c "cd /usr/app/dbt/raffle_shop && dbt-ol run --select query --target postgres2"
         """,
     )
