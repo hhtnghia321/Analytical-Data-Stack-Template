304d0555cadf4e4f2a7bbeefa54452286ef98d73
hhtnghia321 <38842210+hhtnghia321@users.noreply.github.com>
2025-04-23 17:38:10 +0700

    Update sample_dbt_pipeline.py

--- Airflow/dags/sample_dbt_pipeline.py
+++ Airflow/dags/sample_dbt_pipeline.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from datetime import datetime
+from datetime import datetime, timedelta
 
 default_args = {
     "start_date": datetime(2023, 1, 1),
@@ -11,6 +11,8 @@ with DAG("dbt_run_in_project", schedule_interval=None, default_args=default_args
     dbt_run = BashOperator(
         task_id="run_dbt",
         bash_command="""
-        docker exec dbt_trino bash -c "cd /usr/app/dbt/raffle_shop && dbt-ol run --select query --target postgres2"
+        docker exec dbt_trino bash -c "cd /usr/app/dbt/raffle_shop && dbt run --select query --target postgres2 --debug" > /tmp/dbt_output.log 2>&1 &
+        tail -f /tmp/dbt_output.log
         """,
+        execution_timeout=timedelta(minutes=60),  # Adjust timeout as needed
     )
