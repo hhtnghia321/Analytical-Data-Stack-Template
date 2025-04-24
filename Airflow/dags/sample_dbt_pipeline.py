393bd3618db338e00417ad310d07bcc95d61918b
hhtnghia321 <38842210+hhtnghia321@users.noreply.github.com>
2025-04-24 11:10:18 +0700

    Update sample_dbt_pipeline.py

--- Airflow/dags/sample_dbt_pipeline.py
+++ Airflow/dags/sample_dbt_pipeline.py
@@ -11,7 +11,7 @@ with DAG("dbt_run_in_project", schedule_interval=None, default_args=default_args
     dbt_run = BashOperator(
         task_id="run_dbt",
         bash_command="""
-        docker exec dbt_trino bash -c "cd /usr/app/dbt/raffle_shop && dbt run --select query --target postgres2 --debug" > /tmp/dbt_output.log 2>&1 &
+        dbt-ol run --select query --target postgres2 --debug" > /tmp/dbt_output.log 2>&1 &
         tail -f /tmp/dbt_output.log
         """,
         execution_timeout=timedelta(minutes=60),  # Adjust timeout as needed
